def5dc69b92a5210891b3a93d4f1e616d5ec3c35
Tahmina <Tahmina@Tahminas-MacBook-Air.local>
2020-03-24 12:36:40 -0400

    homework1

---
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -0,0 +1,29 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import com.cbt.utilities.StringUtility;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationTests {
+    public static void main(String[] args) {
+        runTest("chrome");
+        runTest("firefox");
+        runTest("safari");
+
+
+    }
+
+    public static void runTest(String browsetype) {
+        WebDriver driver = BrowserFactory.getDriver(browsetype);
+        driver.get("https://google.com");
+        String googleExpectedTitle = driver.getTitle();
+        driver.navigate().to("https://etsy.com");
+        String etsyExpectedTtile = driver.getTitle();
+        driver.navigate().back();
+        StringUtility.verifyEquals(googleExpectedTitle, driver.getTitle());
+        driver.navigate().forward();
+        StringUtility.verifyEquals(etsyExpectedTtile, driver.getTitle());
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,29 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BrowserFactory {
+    public static WebDriver getDriver(String browserName) {
+        WebDriver driver = null;
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "safari":
+
+              driver= new SafariDriver();
+              break;
+
+        }
+        return driver;
+    }
+}
